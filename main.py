257821d5a11ed7f8ce1db27d1365f227ea7673f5
Hessaalajmi <142667801+Hessaalajmi@users.noreply.github.com>
2023-09-06 19:26:55 +0300

    cw-6

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 # write your code here
+person = {
+            "name" : "hessa",
+            "age" : 14,
+            "hobbies" : ['coding', 'archery', 'swimming']
+}
+
+print(person.get('name'))
+print(person.get('age'))
+
+person['age'] = 15
+person['country'] = 'kuwait'
+print(person)
+length = len(person)
+print(length)
+
+person['hobbies']
+
+def check_hobbies(person):
+    if len(person['hobbies']) >= 3:
+        print('YOU ARE AMAZING')
+    else:
+        False
+
+check_hobbies(person)
